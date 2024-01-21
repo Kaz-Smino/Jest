20ebb633f1794128c1f542917f368d5610017a30
Kaz Smino <kaz.sminoo@gmail.com>
2024-01-21 14:45:42 -0800

    Add Obvious Implementationof buzz test

--- fizz_buzz.js
+++ fizz_buzz.js
@@ -2,6 +2,9 @@ function fizz_buzz(number) {
   if (number % 3 === 0) {
       return 'fizz';
   }
+  else if (number % 5 === 0) {
+      return 'buzz';
+  }
 }
 
 module.exports = fizz_buzz;
--- fizz_buzz.test.js
+++ fizz_buzz.test.js
@@ -7,3 +7,7 @@ test('3 convert to fizz' , () => {
 test('6 convert to fizz' , () => {
   expect(fizz_buzz(6)).toBe('fizz');
 });
+
+test('5 convert to buzz' , () => {
+  expect(fizz_buzz(5)).toBe('buzz');
+});
