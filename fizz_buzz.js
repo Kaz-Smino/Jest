994568eca12ea0505b3bdf3e62b618060f243738
Kaz Smino <kaz.sminoo@gmail.com>
2024-01-21 14:48:43 -0800

    Add Obvious Implementationof fizz_buzz test

--- fizz_buzz.js
+++ fizz_buzz.js
@@ -1,5 +1,8 @@
 function fizz_buzz(number) {
-  if (number % 3 === 0) {
+  if (number % 15 === 0) {
+      return 'fizz_buzz';
+  }
+  else if (number % 3 === 0) {
       return 'fizz';
   }
   else if (number % 5 === 0) {
--- fizz_buzz.test.js
+++ fizz_buzz.test.js
@@ -1,13 +1,16 @@
 const fizz_buzz = require('./fizz_buzz');
 
+// fizz
 test('3 convert to fizz' , () => {
   expect(fizz_buzz(3)).toBe('fizz');
 });
 
-test('6 convert to fizz' , () => {
-  expect(fizz_buzz(6)).toBe('fizz');
-});
-
+// buzz
 test('5 convert to buzz' , () => {
   expect(fizz_buzz(5)).toBe('buzz');
+});
+
+// fizz_buzz
+test('15 convert to fizz_buzz' , () => {
+  expect(fizz_buzz(15)).toBe('fizz_buzz');
 });
